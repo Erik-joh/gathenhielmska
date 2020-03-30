7dcb0426f67fee2901ae30678da5856294800615
Erik-joh <erik.johannesson0@gmail.com>
2020-03-30 16:09:34 +0200

    fixed javascript for newsArrow

--- public/themes/Gathenhielmska/functions.php
+++ public/themes/Gathenhielmska/functions.php
@@ -49,5 +49,5 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_script('jquery');
     wp_enqueue_script('bootstrap');
     wp_enqueue_style('style', get_stylesheet_directory_uri() . '/assets/styles/app.css');
-    wp_enqueue_script('script', get_template_directory_uri() . '/assets/scripts/app.js');
+    wp_enqueue_script('script', get_template_directory_uri() . '/assets/scripts/app.js', [], false, true);
 });
--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,14 +1,2 @@
-const frontPage = document.querySelector("body");
-const newsArticles = document.querySelectorAll(".news-article");
-console.log(newsArticles);
-console.log(frontPage);
-const NewsButton = () => {
-  newsArticles.forEach(newsArticle => {
-    newsArrow = newsArticle.firstChild.lastChild;
-    newsArrow.addEventListener("click", () => {
-      console.log("hej");
-      newsArticle.classList.toggle("news-article-tall");
-    });
-  });
-};
+import NewsButton from "./newsButton";
 NewsButton();
