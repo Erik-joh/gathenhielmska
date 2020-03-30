6a61026b32415e20e0b891f119cfa0cbbabdbd54
Michaela Lundborg <michaelalundborg@hotmail.com>
2020-03-30 14:44:34 +0200

    events.php + scss

--- public/themes/Gathenhielmska/page-templates/events.php
+++ public/themes/Gathenhielmska/page-templates/events.php
@@ -1,1 +1,18 @@
 <?php /* Template Name: Events */ ?>
+
+<?php get_header(); ?>
+
+<?php if (have_posts()) : ?>
+
+    <?php while (have_posts()): the_post(); ?>
+
+    <div class="hero">
+        <div class="hero-img"><?php the_content(); ?></div>
+        <h1 class="page-title"><?php the_title(); ?></h1>
+    </div>
+
+    <?php endwhile; ?>
+
+<?php endif; ?>
+
+<?php get_footer(); ?>
