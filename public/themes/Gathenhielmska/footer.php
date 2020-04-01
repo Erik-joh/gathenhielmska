92d7693b17ce4da6fa3bebdda82bb815e9e86f4d
Michaela Lundborg <michaelalundborg@hotmail.com>
2020-04-01 17:37:57 +0200

    single-event + footer

--- public/themes/Gathenhielmska/footer.php
+++ public/themes/Gathenhielmska/footer.php
@@ -1,3 +1,13 @@
+<footer>
+    <div>
+        <ul>
+            <li>gathenhielmska@gmail.com</li>
+            <li>076-921 11 07</li>
+            <li>Stigbergstorget 7, 414 63 Göteborg</li>
+        </ul>
+    </div>
+</footer>
+
   <?php wp_footer(); ?>
 </body>
 </html>
