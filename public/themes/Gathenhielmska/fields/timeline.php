e9884797fca4b81dd683dbbf774efb6045331818
Erik-joh <erik.johannesson0@gmail.com>
2020-04-07 14:46:07 +0200

    started on the about-page

---
+++ public/themes/Gathenhielmska/fields/timeline.php
@@ -0,0 +1,66 @@
+<?php
+if (function_exists('acf_add_local_field_group')) :
+
+    acf_add_local_field_group(array(
+        'key' => 'group_5e8c679ddd71c',
+        'title' => 'Timeline',
+        'fields' => array(
+            array(
+                'key' => 'field_5e8c67aa79107',
+                'label' => 'Year',
+                'name' => 'year',
+                'type' => 'text',
+                'instructions' => '',
+                'required' => 1,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+                'prepend' => '',
+                'append' => '',
+                'maxlength' => '',
+            ),
+            array(
+                'key' => 'field_5e8c67d079108',
+                'label' => 'Description',
+                'name' => 'description',
+                'type' => 'textarea',
+                'instructions' => '',
+                'required' => 1,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+                'maxlength' => '',
+                'rows' => 8,
+                'new_lines' => '',
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'timeline-post',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+    ));
+
+endif;
--- public/themes/Gathenhielmska/page-templates/about.php
+++ public/themes/Gathenhielmska/page-templates/about.php
@@ -2,8 +2,63 @@
 
 <?php get_header(); ?>
 <main class="about-page">
-    <img class="hero-img" src="<?php //the_field('hero_image') ?>" />
-    <h1>Om huset och verksamheten</h1>
+    <img class="hero-img" src="<?php the_field('hero_image') ?>" />
+    <h1><?php the_field('hero_title') ?></h1>
+    <div class="paragraph-1-container">
+        <h3><?php the_field('subheading_1') ?></h3>
+        <p><?php the_field('paragraph_1') ?></p>
+    </div>
+    <div class="paragraph-2-container">
+        <h3><?php the_field('subheading_2') ?></h3>
+        <p><?php the_field('paragraph_2') ?></p>
+    </div>
+    <div>
+        <h2>Historisk tidslinje</h2>
+        <div class="timeline-container">
+            <?php
+            $args = [
+                'post_type' => 'timeline-post'
+            ];
+
+            $posts = get_posts($args);
+            ?>
+            <div class="year-container">
+                <?php foreach ($posts as $post) : ?>
+                    <h4><?php the_field('year') ?></h4>
+                <?php endforeach; ?>
+            </div>
+            <div class="timeline"></div>
+            <div class="description-container">
+                <?php foreach ($posts as $post) : ?>
+                    <p><?php the_field('description') ?></p>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </div>
+    <div class="occupations-container">
+        <h2><?php the_field('occupations_heading') ?></h2>
+        <p><?php the_field('occupations_paragraph') ?></p>
+
+        <div>
+            <?php
+            $args = [
+                'numberposts' => 4,
+                'post_type' => 'occupation',
+            ];
+
+            $occupations = get_posts($args);
+            ?>
+            <?php foreach ($occupations as $post) : ?>
+                <p><?php the_field('occupation') ?></p>
+                <h1><?php the_field('name') ?></h1>
+                <?php if (get_field('link_to_website')) : ?>
+                    <a href="<?php the_field('link_to_website') ?>">
+                        <?php the_field('link_to_website') ?>
+                    </a>
+                <?php endif; ?>
+            <?php endforeach; ?>
+        </div>
+    </div>
 
 </main>
 <?php get_footer(); ?>
