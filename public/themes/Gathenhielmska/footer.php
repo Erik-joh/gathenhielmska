cb4ac6cfb792184d73df93ad65533ae373e18adb
Michaela Lundborg <michaelalundborg@hotmail.com>
2020-04-13 13:49:43 +0200

    footer

--- public/themes/Gathenhielmska/footer.php
+++ public/themes/Gathenhielmska/footer.php
@@ -2,12 +2,12 @@
     <div class="footer">
 
         <div class="to-top">
-            <h4>Till toppen </h4>
-            <img src="<?php bloginfo('template_directory') ?>/assets/images/arrow-up.svg" alt=""></<img>
+            <a class="to-top-link" href="#"><h4>Till toppen</h4></a>
+            <img src="<?php bloginfo('template_directory') ?>/assets/images/arrow-up.svg"></<img>
         </div>
 
         <div>
-            <img class="footer-logo" src="<?php bloginfo('template_directory') ?>/assets/images/footer-logo-mobile.svg" alt="">
+            <img class="footer-logo" src="<?php bloginfo('template_directory') ?>/assets/images/footer-logo-mobile.svg" alt="Gathenhielmska logotype">
             <p>En grundtanke med verksamheten i Gathenhielmska Huset är att skapa processer som
                 uppmuntrar innovativa möten och samverkan mellan konstnärer, kulturutövare,
                 lokalt näringsliv och besökare.</p>
@@ -16,24 +16,39 @@
         <div class="follow">
             <ul>
                 <h4>Följ oss</h4>
-                <li>Facebook</li>
-                <li>Instagram</li>
+                <li>
+                    <img src="<?php bloginfo('template_directory') ?>/assets/images/facebook_icon.svg" alt="Facebook icon">
+                    <a href="https://www.facebook.com/">Facebook</a>
+                </li>
+                <li>
+                    <img src="<?php bloginfo('template_directory') ?>/assets/images/instagram_icon.svg" alt="Instagram icon">
+                    <a href="https://www.instagram.com/gathenhielmska_huset/">Instagram</a>
+                </li>
             </ul>
         </div>
 
         <div class="contact">
             <ul>
                 <h4>Kontakt</h4>
-                <li>gathenhielmska@gmail.com</li>
-                <li>076-921 11 07</li>
-                <li>Stigbergstorget 7, 414 63 Göteborg</li>
+                <li>
+                    <img src="<?php bloginfo('template_directory') ?>/assets/images/mail_icon.svg" alt="Email icon">
+                    <a href="mailto:gathenhielmska@gmail.com">gathenhielmska@gmail.com</a>
+                </li>
+                <li>
+                    <img src="<?php bloginfo('template_directory') ?>/assets/images/phone_icon.svg" alt="Phone icon">
+                    <a href="tel:076-921-11-07">076-921 11 07</a>
+                </li>
+                <li>
+                    <img src="<?php bloginfo('template_directory') ?>/assets/images/location_icon.svg" alt="Location icon">
+                    <span>Stigbergstorget 7, 414 63 Göteborg</span>
+                </li>
             </ul>
         </div>
 
         <div class="footer-logos">
-            <img src="<?php bloginfo('template_directory') ?>/assets/images/studie-framjandet.png" alt="">
-            <img src="<?php bloginfo('template_directory') ?>/assets/images/higab.png" alt="">
-            <img src="<?php bloginfo('template_directory') ?>/assets/images/gbg-stad.png" alt="">
+            <img src="<?php bloginfo('template_directory') ?>/assets/images/studie-framjandet.png" alt="Studiefrämjandet logotype">
+            <img src="<?php bloginfo('template_directory') ?>/assets/images/higab.png" alt="Higab logotype">
+            <img src="<?php bloginfo('template_directory') ?>/assets/images/gbg-stad.png" alt="Göteborgs Stad logotype">
         </div>
 
     </div>
