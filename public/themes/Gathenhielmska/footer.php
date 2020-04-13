b1c22651a3604a250d41450088e4faa8923f9d86
Michaela Lundborg <michaelalundborg@hotmail.com>
2020-04-13 12:53:55 +0200

    footer

--- public/themes/Gathenhielmska/footer.php
+++ public/themes/Gathenhielmska/footer.php
@@ -1,10 +1,41 @@
 <footer>
-    <div>
-        <ul>
-            <li>gathenhielmska@gmail.com</li>
-            <li>076-921 11 07</li>
-            <li>Stigbergstorget 7, 414 63 Göteborg</li>
-        </ul>
+    <div class="footer">
+
+        <div class="to-top">
+            <h4>Till toppen </h4>
+            <img src="<?php bloginfo('template_directory') ?>/assets/images/arrow-up.svg" alt=""></<img>
+        </div>
+
+        <div>
+            <img class="footer-logo" src="<?php bloginfo('template_directory') ?>/assets/images/footer-logo-mobile.svg" alt="">
+            <p>En grundtanke med verksamheten i Gathenhielmska Huset är att skapa processer som
+                uppmuntrar innovativa möten och samverkan mellan konstnärer, kulturutövare,
+                lokalt näringsliv och besökare.</p>
+        </div>
+
+        <div class="follow">
+            <ul>
+                <h4>Följ oss</h4>
+                <li>Facebook</li>
+                <li>Instagram</li>
+            </ul>
+        </div>
+
+        <div class="contact">
+            <ul>
+                <h4>Kontakt</h4>
+                <li>gathenhielmska@gmail.com</li>
+                <li>076-921 11 07</li>
+                <li>Stigbergstorget 7, 414 63 Göteborg</li>
+            </ul>
+        </div>
+
+        <div class="footer-logos">
+            <img src="<?php bloginfo('template_directory') ?>/assets/images/studie-framjandet.png" alt="">
+            <img src="<?php bloginfo('template_directory') ?>/assets/images/higab.png" alt="">
+            <img src="<?php bloginfo('template_directory') ?>/assets/images/gbg-stad.png" alt="">
+        </div>
+
     </div>
 </footer>
 
--- public/themes/Gathenhielmska/functions.php
+++ public/themes/Gathenhielmska/functions.php
@@ -74,5 +74,5 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_script('bootstrap');
     wp_enqueue_style('style', get_stylesheet_directory_uri() . '/assets/styles/app.css');
     wp_enqueue_script('script', get_template_directory_uri() . '/assets/scripts/app.js', [], false, true);
-    wp_enqueue_style('fonts', '//fonts.googleapis.com/css2?family=Lato:wght@700&display=swap');
+    wp_enqueue_style('fonts', '//fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap');
 });
